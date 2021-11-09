169f20c31772158e919ac0c1b75cb0e9235538d1
Leonardo Carvalho Gomes <89273735+leonardoC23111999@users.noreply.github.com>
2021-11-09 11:08:32 -0300

    [@LeoCarvalhoG - Task: MODIFY routing.module.ts ] 2021.11.09

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 
 const routes: Routes = [
@@ -7,7 +8,7 @@ const routes: Routes = [
   {path: '', redirectTo: 'entrar', pathMatch: 'full'},
  
   {path: 'entrar', component: EntrarComponent},
-  {path: 'cadastrar', component: EntrarComponent},
+  {path: 'cadastrar', component: CadastrarComponent},
 
 
 ];
@@ -15,5 +16,6 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+
 })
 export class AppRoutingModule { }
