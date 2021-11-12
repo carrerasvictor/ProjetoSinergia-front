5402f193b891dd6dcc75d0b32c53650614caf112
João vitor <joaovitordiasdasilva1@uni9.edu.br>
2021-11-12 09:51:38 -0300

    [@joaovitordiasdasilva - Task 11: ADD Entrar-Cadastrar] 2021.11.12

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CredenciaisDTO } from '../model/CredenciaisDTO';
+import { Usuario } from '../model/Usuario';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  entrar(usuarioLogin: CredenciaisDTO ): Observable<CredenciaisDTO>{
+    return this.http.post<CredenciaisDTO>('https://sinergiasocial.herokuapp.com/usuarios/login', usuarioLogin)
+  }
+
+  cadastrar(usuario: Usuario): Observable<Usuario>{
+    return this.http.post<Usuario>('https://sinergiasocial.herokuapp.com/usuarios/cadastro', usuario)
+  }
+
 }
