5cce31f180ad114c883000c72db5dd7ecb5fbc7d
davansep <priscila.davanse@gmail.com>
2021-11-12 17:43:48 -0300

    [@davansep - Task 11: MODIFY model Usuario] 2021.11.12

--- src/app/model/Usuario.ts
+++ src/app/model/Usuario.ts
@@ -1,11 +1,11 @@
-import { Postagem } from "./Postagem"
+import { Postagem } from './Postagem';
 
-export class Usuario{
-    public idUsuario: number
-    public nomeCompleto: string
-    public email: string
-    public senha: string
-    public tipo: string
-    public foto: string
-    public minhasPostagens: Postagem
+export class Usuario {
+  public idUsuario: number;
+  public nomeCompleto: string;
+  public email: string;
+  public senha: string;
+  public tipo: string;
+  public foto: string;
+  public minhasPostagens: Postagem[];
 }
