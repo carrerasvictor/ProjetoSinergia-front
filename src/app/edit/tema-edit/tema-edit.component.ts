800a9c043f12b5c4c924232b7458c4df605d1f6a
Amanda <amandaamorim.ctt@gmail.com>
2021-11-14 16:04:15 -0300

    [@amandioca - Task: ADD findByTema by id and atualizar methods] 2021.11.14

--- src/app/edit/tema-edit/tema-edit.component.ts
+++ src/app/edit/tema-edit/tema-edit.component.ts
@@ -1,3 +1,7 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { TemaService } from './../../service/tema.service';
+import { environment } from './../../../environments/environment.prod';
+import { Tema } from './../../model/Tema';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemaEditComponent implements OnInit {
 
-  constructor() { }
+  tema: Tema = new Tema()
 
-  ngOnInit(): void {
+  constructor(
+      private temaService: TemaService,
+      private router: Router,
+      private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    if (environment.token == '') {
+      alert('Sua sessão expirou! Faça login novamente');
+      this.router.navigate(['/entrar']);
+    }
+
+    let id = this.route.snapshot.params['id']
+    this.findByTema(id)
   }
 
+  findByTema(id: number) {
+    this.temaService.getbyIdTema(id).subscribe((resp: Tema) => {
+    this.tema = resp
+    })
+
+  }
+
+  atualizar() {
+    this.temaService.putTema(this.tema).subscribe((resp: Tema) =>{
+      this.tema = resp
+      alert('Tema atualizado!')
+      this.router.navigate(['/tema'])
+    })
+  }
+  
 }
