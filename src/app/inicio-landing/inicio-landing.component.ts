6f4d6f910307f8288a7a70c0980bb76d03557d27
João vitor <joaovitordiasdasilva1@uni9.edu.br>
2021-11-19 10:23:43 -0300

    [@joaovitordiasdasilva - UPDATE ] 2021.11.19

--- src/app/inicio-landing/inicio-landing.component.ts
+++ src/app/inicio-landing/inicio-landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-inicio-landing',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioLandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth:AuthService
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.auth.emptyToken()
   }
 
 }
