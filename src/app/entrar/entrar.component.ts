ca09b3b0e09e5d95a73b18f8e0522b9a21d27bf5
Amanda Amorim <amandaamorim.ctt@gmail.com>
2021-11-23 18:33:08 -0300

    Revert "Mensagem"

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -23,33 +23,26 @@ export class EntrarComponent implements OnInit {
     window.scroll(0, 0), this.auth.emptyToken();
   }
 
-  
-
   entrar() {
+    this.auth.entrar(this.usuarioLogin).subscribe(
+      (resp: CredenciaisDTO) => {
+        this.usuarioLogin = resp;
 
-    this.auth.entrar(this.usuarioLogin).subscribe((resp: CredenciaisDTO) => {
-      this.usuarioLogin = resp;
+        environment.token = this.usuarioLogin.token;
+        environment.idUsuario = this.usuarioLogin.idUsuario;
+        environment.token = this.usuarioLogin.token;
+        environment.nomeCompleto = this.usuarioLogin.nome;
+        environment.foto = this.usuarioLogin.foto;
+        environment.tipo = this.usuarioLogin.tipo;
 
-      environment.token = this.usuarioLogin.token
-      environment.idUsuario = this.usuarioLogin.idUsuario
-      environment.token = this.usuarioLogin.token
-      environment.nomeCompleto = this.usuarioLogin.nome
-      environment.foto = this.usuarioLogin.foto
-      environment.tipo = this.usuarioLogin.tipo
-
-      //console.log(environment.token)
-      this.router.navigate(['/inicio']);
-    }, erro => {
-      if (erro.status == 400) {
-        alert("Usuário ou senha inválidos!");
+        //console.log(environment.token)
+        this.router.navigate(['/inicio']);
+      },
+      (erro) => {
+        if (erro.status == 400) {
+          this.alertas.showAlertDanger('Usuário ou senha inválidos!');
+        }
       }
-
-      if (environment.tipo != 'adm'){
-        alert("Você precisa ser adm para acessar essa rota");
-        this.router.navigate(['/inicio'])
-      }
-      
-    })
+    );
   }
 }
-
